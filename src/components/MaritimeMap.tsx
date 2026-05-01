d6fc36516c9d313c8b830f6dce8eb255e18b7c23
abhi <abhijith.uwc.rw@gmail.com>
2026-05-01 13:37:38 -0400

    Fix: Resolve CSS module type errors and finalize build

--- src/components/MaritimeMap.tsx
+++ src/components/MaritimeMap.tsx
@@ -39,13 +39,12 @@ export const MaritimeMap = () => {
           scrollWheelZoom={true} 
           style={{ height: '600px', width: '100%', borderRadius: '16px' }}
         >
-          {/* High-end Dark Tiles */}
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
             url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
           />
 
-          <Polyline positions={voyagePath} color="#2dd4bf" weight={2} dashArray="5, 10" />
+          <Polyline positions={voyagePath} pathOptions={{ color: '#2dd4bf', weight: 2, dashArray: '5, 10' }} />
 
           {strikes.map((s) => (
             <Marker key={s.id} position={s.pos} icon={oceanIcon}>
